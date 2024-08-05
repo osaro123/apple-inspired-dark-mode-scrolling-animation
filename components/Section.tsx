6c179972a9f91afa962cdadf35af07fe7c7b5692
osaro123 <osarolawani983@gmail.com>
2024-08-05 21:38:08 +0100

    initial commit

---
+++ components/Section.tsx
@@ -0,0 +1,26 @@
+import { useMotionValueEvent, useScroll } from 'framer-motion'
+import React, { ReactNode, useRef } from 'react'
+
+const Section:React.FC<{
+    theme: string
+    setTheme: (value: string) => void
+    children: ReactNode
+}> = ({children,theme,setTheme}) => {
+    const container = useRef(null)
+    const {scrollYProgress} = useScroll({
+        target: container,
+        offset: ["start center","end center"]
+    })
+    useMotionValueEvent(scrollYProgress,"change",(value) => {
+        if(value > 0 && value < 1){
+            setTheme(theme)
+        }
+    })
+  return (
+    <section ref={container} className={`bg-white dark:bg-black p-8 min-h-screen flex flex-col`}>
+        {children}
+    </section>
+  )
+}
+
+export default Section
